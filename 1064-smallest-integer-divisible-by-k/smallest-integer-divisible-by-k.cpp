e34841182b34d5c7f7b7728672858798345bfe29
naish-jaiswal-25 <naish.jaiswal.25@gmail.com>
2025-11-25 21:51:08 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (22.83%) - LeetSync

--- 1064-smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.cpp
+++ 1064-smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.cpp
@@ -3,7 +3,7 @@ public:
     int smallestRepunitDivByK(int k) {
         if(k%2==0 || k%5==0) return -1;  
         int answer=0;
-        for(int i=1;i<=1000000;i++){
+        for(int i=1;i<=k;i++){
             answer=(answer*10+1)%k;
             if(answer==0) return i;
         }     
