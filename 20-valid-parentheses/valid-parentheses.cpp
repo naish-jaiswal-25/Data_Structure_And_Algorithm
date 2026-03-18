6b59d43b0154a38b82bcc6908eb26b3b8b936626
naish-jaiswal-25 <naish.jaiswal.25@gmail.com>
2026-03-18 23:47:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (67.04%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if (s.length() % 2 != 0) return false;
+
+        stack<char> st;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                st.push(s[i]);
+            } else {
+                if (st.empty()) return false;
+
+                char top = st.top();
+                if ((s[i] == ')' && top == '(') ||
+                    (s[i] == '}' && top == '{') ||
+                    (s[i] == ']' && top == '[')) {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return st.empty();
+    }
+};
