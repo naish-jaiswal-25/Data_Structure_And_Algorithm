54374a5d1551092d3737eebff83e027787f4f52c
naish-jaiswal-25 <naish.jaiswal.25@gmail.com>
2026-04-11 22:27:58 +0530

    Time: 4 ms (41.49%) | Memory: 146.7 MB (90.53%) - LeetSync

---
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (!root) return 0;
+
+        queue<TreeNode*> q;
+        q.push(root);
+        int depth = 1;
+
+        while (!q.empty()) {
+            int size = q.size();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front(); q.pop();
+
+                // first leaf found
+                if (!node->left && !node->right)
+                    return depth;
+
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+            depth++;
+        }
+        return depth;
+    }
+};
