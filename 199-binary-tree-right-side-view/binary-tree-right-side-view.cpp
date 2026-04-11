972bc813dd12d37fbbc90a7695a927a7af4f481c
naish-jaiswal-25 <naish.jaiswal.25@gmail.com>
2026-04-11 22:15:23 +0530

    Time: 4 ms (2.27%) | Memory: 14.9 MB (85.20%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void dfs(TreeNode* root, int level, vector<int>& ans) {
+        if (!root) return;
+
+        if (level == ans.size())
+            ans.push_back(root->val);
+
+        dfs(root->right, level + 1, ans);
+        dfs(root->left, level + 1, ans);
+    }
+
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans;
+        dfs(root, 0, ans);
+        return ans;
+    }
+};
