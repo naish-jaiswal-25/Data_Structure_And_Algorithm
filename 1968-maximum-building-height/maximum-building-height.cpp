88ad6fb7a5f29a2f8114bcffd36726d9bba0588a
naish-jaiswal-25 <naish.jaiswal.25@gmail.com>
2026-07-01 19:29:21 +0530

    Time: 47 ms (90.98%) | Memory: 106.6 MB (50.06%) - LeetSync

--- 1968-maximum-building-height/maximum-building-height.cpp
+++ 1968-maximum-building-height/maximum-building-height.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int maxBuilding(int n, vector<vector<int>>& restrictions) {
         restrictions.push_back({1, 0});
+        restrictions.push_back({n, n - 1});
+
         sort(restrictions.begin(), restrictions.end());
 
         int m = restrictions.size();
@@ -11,7 +13,7 @@ public:
             restrictions[i][1] = min(
                 restrictions[i][1],
                 restrictions[i - 1][1] +
-                (restrictions[i][0] - restrictions[i - 1][0])
+                    restrictions[i][0] - restrictions[i - 1][0]
             );
         }
 
@@ -20,26 +22,21 @@ public:
             restrictions[i][1] = min(
                 restrictions[i][1],
                 restrictions[i + 1][1] +
-                (restrictions[i + 1][0] - restrictions[i][0])
+                    restrictions[i + 1][0] - restrictions[i][0]
             );
         }
 
         int ans = 0;
 
-        // Maximum height between consecutive restricted buildings
         for (int i = 1; i < m; i++) {
             int x1 = restrictions[i - 1][0];
             int h1 = restrictions[i - 1][1];
             int x2 = restrictions[i][0];
             int h2 = restrictions[i][1];
 
-            ans = max(ans, (h1 + h2 + (x2 - x1)) / 2);
+            int d = x2 - x1;
+            ans = max(ans, (h1 + h2 + d) / 2);
         }
-
-        // Buildings after the last restriction
-        ans = max(ans,
-                  restrictions.back()[1] +
-                  (n - restrictions.back()[0]));
 
         return ans;
     }
